78ff87361992604fcfe828f865a6eb1decd1d231
Jannik Wempe <jannikwempe@gmail.com>
2020-02-27 07:33:35 +0100

    add lines that will be overriden by .editorconfig

--- javascript.exclude/.prettierrc.js
+++ javascript.exclude/.prettierrc.js
@@ -1,6 +1,10 @@
 // https://prettier.io/docs/en/options.html
 module.exports = {
+  // printWidth: 100, // overriden by .editorconfig
+  // useTabs: false, // overriden by .editorconfig
+  // tabWidth: 2, // overriden by .editorconfig
   singleQuote: false,
+  // endOfLine: "lf", // overriden by .editorconfig
   semi: true,
   trailingComma: "es5",
   rcVerbose: true,
