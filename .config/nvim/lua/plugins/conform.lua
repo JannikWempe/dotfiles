66467c0cb2f0166f8cfd6c0ee0376b083a106fb1
Jannik Wempe <jannik@wempe.dev>
2025-12-17 20:40:12 +0100

    use prettierd and eslintd

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,7 +2,18 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      default_format_opts = { timeout_ms = 60000 },
+      formatters_by_ft = {
+        javascript = { "prettierd", "eslint_d" },
+        javascriptreact = { "prettierd", "eslint_d" },
+        typescript = { "prettierd", "eslint_d" },
+        typescriptreact = { "prettierd", "eslint_d" },
+        json = { "prettierd" },
+        jsonc = { "prettierd" },
+        markdown = { "prettierd" },
+        css = { "prettierd" },
+        html = { "prettierd" },
+        yaml = { "prettierd" },
+      },
     },
   },
 }
